983e156555e1b97ea8fee478ffa88f3457efe06a
willytedjakusuma-moxi <willybudiman.tedjakusuma@moxiworks.com>
2025-04-14 16:37:16 +0800

    feat: Initial Commit

---
+++ config/routes.cr
@@ -0,0 +1,18 @@
+Amber::Server.configure do
+  pipeline :api do
+    # plug Amber::Pipe::PoweredByAmber.new
+    plug Amber::Pipe::Error.new
+    plug Amber::Pipe::Logger.new
+    plug Amber::Pipe::Session.new
+    plug Amber::Pipe::CORS.new
+  end
+
+  routes :api do
+  end
+
+  routes :static do
+    # Each route is defined as follow
+    # verb resource : String, controller : Symbol, action : Symbol
+    get "/*", Amber::Controller::Static, :index
+  end
+end
